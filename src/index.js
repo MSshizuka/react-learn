6d20e700e90408a8c637444154341d0d2d081e37
wangbowen <wangbowen@xinshiyun.com>
2021-01-05 11:10:31 +0800

    Vote组件

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Vote from './Vote';
+
+/* 导入公共样式 */
+import './assets/base.css';
 
 ReactDOM.render(
   <div>
-
+    <Vote title='海贼王2021年完结!' supNum={10} oppNum={20}/>
   </div>,
   document.getElementById('root')
 );
